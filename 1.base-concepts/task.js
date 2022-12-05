31e9b7c08e8d363f51633d48e9ec7b1012c94eeb
Joynie12 <bigw199412@gmail.com>
2022-12-05 10:23:25 +0300

    First Homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let arr = [Math.pow(b , 2) - 4 * a * c];
+  let answer = [];
+  if (arr < 0) {
+    console.log(answer);
+  } else if (arr == 0) {
+    answer = [-b/(2*a)];
+    console.log(answer);
+  } else if (arr > 0) {
+    answer = [(-b + Math.sqrt(arr) )/(2*a) , (-b - Math.sqrt(arr))/(2*a)];
+    console.log(answer);
+  }
+  return answer;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (
+    isNaN(percent) ||
+    isNaN(contribution) ||
+    isNaN(amount) ||
+    isNaN(countMonths)
+    ) {
+       console.log('Один из параметров содержит неверное значение');
+       return false;
+    }
+
+  let percentForMonth = (percent / 100) / 12;
+  let bodyOfCredit = amount - contribution;
+  let payForMonth = bodyOfCredit * (percentForMonth + (percentForMonth / ((Math.pow((1 + percentForMonth), countMonths) - 1))));
+  let totalAmount = 0;
+  totalAmount = parseFloat((payForMonth * countMonths).toFixed(2));
+  return totalAmount;
 }
