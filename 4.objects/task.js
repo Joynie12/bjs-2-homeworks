c7e9f925d9107cb4822c0eae4093e3835761d1bf
Joynie12 <bigw199412@gmail.com>
2022-12-22 09:30:17 +0300

    Fourth commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
 function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+
+  // let firstStudent = new Student(`James`, `Male`, 19, []);
+  // let secondStudent = new Student(`Helen`, `Female`, 22, []);
+  // let thirdStudent = new Student(`John`, `Male`, 20, []);
   
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks != undefined) {
+    this.marks.push(...marks);
+  } else {console.log(`Отчислен`)};
 }
 
 Student.prototype.getAverage = function () {
-  
+  let sum = 0;
+  let avg = 0;
+
+  if (this.marks !== undefined && this.marks.length > 0) {
+    sum = this.marks.reduce((a, b) => a + b, 0);
+    avg = parseFloat((sum/this.marks.length).toFixed(1))
+  } else {console.log(0)};
+
+  return avg;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
 }
